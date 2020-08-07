ce42d6ee966786d77b6935107dd14e5eac427d65
Satoru SATOH <satoru.satoh@gmail.com>
2020-08-07 15:17:34 +0900

    enhancement: add file size and checksum test module and its test cases

---
+++ src/infraspec/files/stat.py
@@ -0,0 +1,65 @@
+#
+# Copyright (C) 2020 Satoru SATOH <satoru.satoh@gmail.com>.
+# SPDX-License-Identifier: MIT
+#
+# ref. https://serverspec.org/resource_types.html#file
+#
+"""file test functions using stat(2) and so on.
+"""
+import enum
+import hashlib
+import pathlib
+import typing
+
+from .common import Path
+
+
+class Checksum(enum.Enum):
+    """File checksum type."""
+
+    MD5 = hashlib.md5
+    SHA1 = hashlib.sha1
+    SHA256 = hashlib.sha256
+    SHA512 = hashlib.sha512
+
+
+def get_checksum(path: Path,
+                 csum_fun: typing.Union[Checksum, None] = None) -> bool:
+    """
+    :return: Checksum value of the file of given path
+    """
+    path = pathlib.Path(path)
+
+    if csum_fun is None:
+        csum_fun = Checksum.SHA256
+
+    return csum_fun.value(path.read_bytes()).hexdigest()
+
+
+def has_checksum(path: Path, csum: str,
+                 csum_fun: typing.Union[Checksum, None] = None) -> bool:
+    """
+    :return: True if the file at the path `path` has given checksum
+    """
+    return get_checksum(path, csum_fun=csum_fun) == csum
+
+
+def get_size(path: Path) -> int:
+    """
+    :param path: The path to target object
+
+    :return: True if the file at the path `path` has given size
+    """
+    return pathlib.Path(path).stat().st_size
+
+
+def has_size(path: Path, size: int) -> bool:
+    """
+    :param path: The path to target object
+    :param size: Expected file size in bytes
+
+    :return: True if the file at the path `path` has given size
+    """
+    return get_size(path) == size
+
+# vim:sw=4:ts=4:et:
---
+++ tests/files/test_stat.py
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2020 Satoru SATOH <satoru.satoh@gmail.com>.
+# SPDX-License-Identifier: MIT
+#
+# pylint: disable=missing-function-docstring
+"""File tests.
+"""
+import pathlib
+
+import infraspec.files.stat as TT
+
+
+# sha256sum for an empty text file
+CSUM = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+
+def test_has_checksum(tmpdir):
+    path = pathlib.Path(tmpdir) / "test.txt"
+    path.touch()
+
+    assert not TT.has_checksum(__file__, CSUM)
+    assert TT.has_checksum(path, CSUM)
+    assert TT.has_checksum(path, CSUM, TT.Checksum.SHA256)
+
+
+def test_has_size(tmpdir):
+    path = pathlib.Path(tmpdir) / "test.txt"
+    path.write_text("hello, world\n")
+    esize = 13
+
+    assert not TT.has_size(__file__, 0)
+    assert TT.has_size(path, esize)
+
+# vim:sw=4:ts=4:et:
