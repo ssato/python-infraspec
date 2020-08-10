a915805812dc79ae16c6d8119ba9ea416ee7bfa3
Satoru SATOH <satoru.satoh@gmail.com>
2020-08-10 14:37:00 +0900

    change: export .selinux.utils.has_selinux_label

--- src/infraspec/selinux/__init__.py
+++ src/infraspec/selinux/__init__.py
@@ -8,5 +8,8 @@ from .selinux_ import (  # noqa: F401
     is_enabled, get_config, get_mode, get_policy_type,
     is_enforcing, is_permissive, is_disabled
 )
+from .utils import (  # noqa: F401
+    has_selinux_label,
+)
 
 # vim:sw=4:ts=4:et:
