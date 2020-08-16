eb4e6f4e6ba72d3c645bfdd6f18209cd88fd56a6
Satoru SATOH <satoru.satoh@gmail.com>
2020-08-16 16:50:04 +0900

    enhancement: add user and group related functions

---
+++ src/infraspec/common/user_group.py
@@ -0,0 +1,65 @@
+#
+# Copyright (C) 2020 Satoru SATOH <satoru.satoh@gmail.com>.
+# SPDX-License-Identifier: MIT
+#
+"""Utility functions to get users info.
+"""
+import functools
+import grp
+import operator
+import pwd
+import typing
+
+
+def get_all_groups_itr() -> typing.Iterator[typing.Mapping]:
+    """
+    Get all groups' info from group data base.
+    """
+    for ginfo in grp.getgrall():
+        yield dict(gid=ginfo.gr_gid, name=ginfo.gr_name,
+                   members=ginfo.gr_mem)
+
+
+@functools.lru_cache(maxsize=8)
+def get_all_groups() -> typing.List[typing.Mapping]:
+    """
+    Get all groups' info from group data base.
+    """
+    return sorted(get_all_groups_itr(), key=operator.itemgetter("name"))
+
+
+def get_user_by_name(name: str,
+                     group: bool = False) -> typing.Optional[typing.Mapping]:
+    """
+    Get user info by user's name from password database.
+    """
+    try:
+        uinfo = pwd.getpwnam(name)
+        ret = dict(uid=uinfo.pw_uid, gid=uinfo.pw_gid, name=uinfo.pw_name,
+                   gecos=uinfo.pw_gecos, homedir=uinfo.pw_dir, groups=[],
+                   shell=uinfo.pw_shell)
+        if group:
+            ret["groups"] = [group["name"] for group in get_all_groups()
+                             if name in group["members"]]
+        return ret
+
+    except KeyError:
+        pass
+
+    return None
+
+
+def get_group_by_name(name: str) -> typing.Optional[typing.Mapping]:
+    """
+    Get group info by group's name from group database.
+    """
+    try:
+        group = grp.getgrnam(name)
+        return dict(gid=group.gr_gid, name=group.gr_name, members=group.gr_mem)
+
+    except KeyError:
+        pass
+
+    return None
+
+# vim:sw=4:ts=4:et:
---
+++ tests/common/test_user_group.py
@@ -0,0 +1,50 @@
+#
+# Copyright (C) 2020 Satoru SATOH <satoru.satoh@gmail.com>.
+# SPDX-License-Identifier: MIT
+#
+# pylint: disable=missing-function-docstring
+#
+"""User and group tests.
+"""
+import os
+import pwd
+
+import infraspec.common.user_group as TT
+
+
+try:
+    USERNAME = os.getlogin()
+except OSError:
+    USERNAME = pwd.getpwuid(os.getuid()).pw_name
+
+
+def test_get_all_groups_itr():
+    assert list(TT.get_all_groups_itr())
+
+
+def test_get_all_groups():
+    res = TT.get_all_groups()
+
+    assert res
+    assert [usr for usr in res if usr.get("gid") == os.getgid()]
+
+
+def test_get_user_by_name():
+    assert TT.get_user_by_name(USERNAME)
+
+
+def test_get_user_by_name_with_group():
+    res = TT.get_user_by_name(USERNAME, group=True)
+
+    assert res
+    assert res["groups"]
+
+
+def test_get_group_by_name():
+    usr = TT.get_user_by_name(USERNAME, group=True)
+    assert usr["groups"]
+
+    for group in usr["groups"]:
+        assert TT.get_group_by_name(group)
+
+# vim:sw=4:ts=4:et:
